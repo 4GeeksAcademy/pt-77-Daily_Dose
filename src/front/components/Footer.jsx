6852a925f9d21bdab34ea7b34c647bdea8136c18
Moisa Estime <moy.estime@gmail.com>
2025-06-17 16:07:40 +0000

    done

--- src/front/components/Footer.jsx
+++ src/front/components/Footer.jsx
@@ -1,29 +1,28 @@
+import React from "react";
+
 export const Footer = () => (
-	<footer className="footer mx-auto text-center h-25">
-		<div className=" mx-auto  bg-dark text-light w-100">
-			<div className="row">
-				<div className=" d-flex mx-auto justify-content-around">
-					<div>About us</div>
-					<div>Products</div>
-					<div>Our Team</div>
-					<div>Get Help</div>
-					<div>Contact</div>
-				</div>
-				<hr></hr>
-				<div className="pt-2 pb-2 text-center">
-					<span>All music used in this content is the property of its respective owners. We do not claim ownership of any copyrighted music featured herein. All rights belong to the original artists and copyright holders.</span>
-					<hr className="bg-light my-2" style={{ height: '1px', border: 'none' }} />
-				</div>
-			</div>
-		</div>
-		<div className="col-4 fs-2 d-flex mx-auto justify-content-around text-light bg-dark w-100">
-			<i class="fa-brands fa-facebook"></i>
-			<i class="fa-brands fa-x-twitter"></i>
-			<i class="fa-brands fa-instagram"></i>
-			<i class="fa-brands fa-google-plus-g"></i>
-			<i class="fa-brands fa-linkedin"></i>
-			<i class="fa-brands fa-pinterest"></i>
-		</div>
-	</footer>
+  <footer className="bg-dark text-light py-3 w-100">
+    <div className="container text-center">
+      <div className="d-flex flex-wrap justify-content-center gap-3 small mb-2">
+        <div>About Us</div>
+        <div>Products</div>
+        <div>Our Team</div>
+        <div>Get Help</div>
+        <div>Contact</div>
+      </div>
+
+      <hr className="bg-light my-2" style={{ height: "1px", border: "none" }} />
+
+      <div className="d-flex justify-content-center gap-3 fs-5">
+        <i className="fa-brands fa-facebook"></i>
+        <i className="fa-brands fa-x-twitter"></i>
+        <i className="fa-brands fa-instagram"></i>
+        <i className="fa-brands fa-google-plus-g"></i>
+        <i className="fa-brands fa-linkedin"></i>
+        <i className="fa-brands fa-pinterest"></i>
+      </div>
+    </div>
+  </footer>
 );
+
 export default Footer;
